5f237f7f54522517fdf998ac685fb70742bd8fe6
Mary Saray <msaray.ms@outlook.com>
2021-07-22 16:32:09 -0700

    Add project files.

---
+++ HotelModelBindingMVC/Models/Hotel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelModelBindingMVC.Models
+{
+    /// <summary>
+    /// Represents one Hotel.
+    /// </summary>
+    public class Hotel
+    {
+        /// <summary>
+        /// Represents the Hotel Name.
+        /// </summary>
+        public string HotelName { get; set; }
+
+        /// <summary>
+        /// Total rooms in the Hotel.
+        /// </summary>
+        public int Rooms { get; set; }
+
+        /// <summary>
+        /// Total booked rooms in the Hotel.
+        /// </summary>
+        public int Booked { get; set; }
+
+        /// <summary>
+        /// Returns amount of room available.
+        /// </summary>
+        /// <returns></returns>
+        public int CheckAvailability()
+        {
+            return this.Rooms - this.Booked;
+        }
+    }
+}
