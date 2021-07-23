7acd9bd40578bca4a0a506d372e6218338e79965
Mary Saray <msaray.ms@outlook.com>
2021-07-22 17:49:07 -0700

    Created add view

---
+++ HotelModelBindingMVC/Controllers/HotelController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelModelBindingMVC.Controllers
+{
+    public class HotelController : Controller
+    {
+        // Add hotel to database
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
