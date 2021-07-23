b062ce76582956c8a1a371be2f6c3c2e79300d28
Mary Saray <msaray.ms@outlook.com>
2021-07-22 19:05:54 -0700

    Added customer controller

---
+++ HotelModelBindingMVC/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using HotelModelBindingMVC.Models;
+
+namespace HotelModelBindingMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        [HttpGet] // associated with the nav-link URL
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost] // retireve data after submission
+        public IActionResult Add(IFormCollection form)
+        {
+            // TODO: server-side valdiation for all data
+
+            // extract data: create new instance of RSVP object
+            RSVP req = new RSVP();
+            req.FirstName = form["first-name"];
+            req.LastName = form["last-name"];
+            req.PhoneNumber = form["phone"];
+            req.Email = form["email"];
+            req.Date = Convert.ToDateTime(form["date"]); // convert to datetime
+
+            // Add customer to database
+            return View();
+        }
+    }
+}
