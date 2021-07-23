1a9b8e33d8e75a2b1fda1c02f5c42dbc11e0068a
Mary Saray <msaray.ms@outlook.com>
2021-07-22 20:46:08 -0700

    Added attribute annotations

--- HotelModelBindingMVC/Models/RSVP.cs
+++ HotelModelBindingMVC/Models/RSVP.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,26 +14,31 @@ namespace HotelModelBindingMVC.Models
         /// <summary>
         /// The legal customer first name
         /// </summary>
+        [Display(Name ="First Name:")]
         public string FirstName { get; set; }
 
         /// <summary>
         /// The legal customer last name
         /// </summary>
+        [Display(Name ="Last Name:")]
         public string LastName { get; set; }
 
         /// <summary>
         /// Contact phone number
         /// </summary>
+        [Display(Name ="Phone Number:")]
         public string PhoneNumber { get; set; }
 
         /// <summary>
         /// Contact email address
         /// </summary>
+        [Display(Name ="Email:")]
         public string Email { get; set; }
 
         /// <summary>
         /// the day form was submitted
         /// </summary>
+        [Display(Name ="Date:")]
         public DateTime Date { get; set; }
     }
 }
